abb6d107b05f11a8377ab2e62aaca5944af6838a
Enydin <nelsonwand@gmail.com>
2021-03-06 19:12:05 +0300

    added delivery prompt

--- js/index.js
+++ js/index.js
@@ -1,40 +1,47 @@
-function Pizza(size, crust, toppings) {
-    this.size = size;
-    this.crust = crust;
-    this.toppings= toppings;
-    this.number = number;
-  }
+//function Pizza(size, crust, toppings,price) {
+//    this.size = size;
+//    this.crust = crust;
+//    this.toppings= toppings;
+//    this.number = number;
+//    this.price = price;
+//  }
 
   $("#checkout").click(function () {
     event.preventDefault();
-    let size = $("#size option:selected").val("");
-    let crust = $("#crust option:selected").val("");
-    let toppings = $("#toppings option:selected").val("");
-    let number = $("#number").val("");
-    //console.log(size,crust,toppings,number);
+    let size = $("#size option:selected").val();
+    let crust = $("#crust option:selected").val();
+    let toppings = $("#toppings option:selected").val();
+    let number = $("#number").val();
+    console.log(size,crust,toppings,number);
 })
 
-function order(size, crust, toppings, number, total) {
-    return { size, crust, toppings, number, total};
+function myOrder(size, crust, toppings, number, totalPrice) {
+    return { size, crust, toppings, number, totalPrice};
 }
-let price, totalPrice;
-Pizza.prototype.yourOrder =function(){
-    size = "small";
-    this.price = 300; 
-   return totalPrice = (price * number)
+   console.log(myOrder);
+   function Size(small, medium, large){
+       this.small = small;
+       this.medium = medium;
+       this.large = large;
+   }
+
+   
+Size.prototype.small =function(){
+    //size = "small";
+    price = 300; 
+   return price * number;
 }
-Pizza.prototype.yourOrder =function(){
-    size = "medium";
-    this.price = 600; 
-   return totalPrice = (price * number)
+Size.prototype.medium =function(){
+    //size = "medium";
+    price = 600; 
+    return price * number;
 }
-Pizza.prototype.yourOrder =function(){
-    size = "large";
-    this.price = 1200; 
-   return totalPrice = (price * number)
+Size.prototype.large =function(){
+    //size = "large";
+    price = 1200; 
+    return price * number;
 }
 
-//var totalPrice = (price*number)
 
 switch (toppings) {
     case toppings = "pepperoni":
@@ -62,4 +69,20 @@ switch (toppings) {
         totalPrice = totalPrice + 80;
         break;
 }
-       console.log(yourOrder); 
+    //console.log(totalPrice); 
+       
+//    let newOrder = myOrder(size, crust, toppings, number, totalPrice);
+//    $('#list').text();
+//    $("#list").append(newOrder.f + "<br>" + "size :   "
+//        + newOrder.s + "<br>" + "crust :     "
+//        + newOrder.c + "<br>" + "toppings :     "
+//        + newOrder.t + "<br>" + " number :    "
+//        + newOrder.n + "<br>" + "totalPrice :  ")
+    $('#list').text();
+    $('#list').append()
+
+
+
+    $(".deliver").click(function () {
+        prompt ("PLEASE INPUT YOUR LOCATION");
+    })
