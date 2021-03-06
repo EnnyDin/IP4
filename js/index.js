78b690febb507a785b81889050f2d1c31ece0d3d
EnnyDin <78684712+EnnyDin@users.noreply.github.com>
2021-03-06 12:07:59 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,17 +1,15 @@
+function Pizza(size, crust, toppings) {
+    this.size = size;
+    this.crust = crust;
+    this.toppings= toppings;
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  $("#checkout").click(function () {
+    event.preventDefault();
+    let size = $("#size option:selected").val();
+    let crust = $("#crust option:selected").val();
+    let toppings = $("#toppings option:selected").val();
+    let number = $("#number").val();
+    console.log(size,crust,toppings,number);
+})
 
